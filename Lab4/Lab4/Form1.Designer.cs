b23150c810e1c9d55e063d02f9616dfa95a71c11
Luilaniel <karinakasko11@gmail.com>
2024-10-01 16:56:36 +0300

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Lab4
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            toolStrip1 = new ToolStrip();
+            btnAdd = new ToolStripButton();
+            btnEdit = new ToolStripButton();
+            tsSeparator1 = new ToolStripSeparator();
+            btnDel = new ToolStripButton();
+            btnClear = new ToolStripButton();
+            tsSeparator2 = new ToolStripSeparator();
+            btnExit = new ToolStripButton();
+            bindSrcCameras = new BindingSource(components);
+            gvCameras = new DataGridView();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)bindSrcCameras).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gvCameras).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnAdd, btnEdit, tsSeparator1, btnDel, btnClear, tsSeparator2, btnExit });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 27);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            btnAdd.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnAdd.Image = (Image)resources.GetObject("btnAdd.Image");
+            btnAdd.ImageTransparentColor = Color.Magenta;
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(29, 24);
+            btnAdd.Text = "Додати запис про камеру";
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEdit.Image = (Image)resources.GetObject("btnEdit.Image");
+            btnEdit.ImageTransparentColor = Color.Magenta;
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(29, 24);
+            btnEdit.Text = "Редагувати запис";
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // tsSeparator1
+            // 
+            tsSeparator1.Name = "tsSeparator1";
+            tsSeparator1.Size = new Size(6, 27);
+            // 
+            // btnDel
+            // 
+            btnDel.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnDel.Image = (Image)resources.GetObject("btnDel.Image");
+            btnDel.ImageTransparentColor = Color.Magenta;
+            btnDel.Name = "btnDel";
+            btnDel.Size = new Size(29, 24);
+            btnDel.Text = "Видалити запис";
+            btnDel.Click += btnDel_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnClear.Image = (Image)resources.GetObject("btnClear.Image");
+            btnClear.ImageTransparentColor = Color.Magenta;
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(29, 24);
+            btnClear.Text = "Очистити дані";
+            btnClear.Click += btnClear_Click;
+            // 
+            // tsSeparator2
+            // 
+            tsSeparator2.Name = "tsSeparator2";
+            tsSeparator2.Size = new Size(6, 27);
+            // 
+            // btnExit
+            // 
+            btnExit.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExit.Image = (Image)resources.GetObject("btnExit.Image");
+            btnExit.ImageTransparentColor = Color.Magenta;
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(29, 24);
+            btnExit.Text = "Вийти з програми";
+            btnExit.Click += btnExit_Click;
+            // 
+            // gvCameras
+            // 
+            gvCameras.AllowUserToAddRows = false;
+            gvCameras.AllowUserToDeleteRows = false;
+            gvCameras.AutoGenerateColumns = false;
+            gvCameras.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gvCameras.DataSource = bindSrcCameras;
+            gvCameras.Dock = DockStyle.Fill;
+            gvCameras.Location = new Point(0, 27);
+            gvCameras.Name = "gvCameras";
+            gvCameras.ReadOnly = true;
+            gvCameras.RowHeadersWidth = 51;
+            gvCameras.Size = new Size(800, 423);
+            gvCameras.TabIndex = 1;
+            gvCameras.CellContentClick += gvCameras_CellContentClick;
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(gvCameras);
+            Controls.Add(toolStrip1);
+            Name = "fMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Лабораторна робота №4";
+            Load += fMain_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)bindSrcCameras).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gvCameras).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton btnAdd;
+        private ToolStripButton btnEdit;
+        private ToolStripSeparator tsSeparator1;
+        private ToolStripButton btnDel;
+        private ToolStripButton btnClear;
+        private ToolStripSeparator tsSeparator2;
+        private ToolStripButton btnExit;
+        private BindingSource bindSrcCameras;
+        private DataGridView gvCameras;
+    }
+}
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,114 @@
+﻿namespace Lab4
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvCameras.AutoGenerateColumns = false;
+
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Brand";
+            column.Name = "Brand";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Model";
+            column.Name = "Model";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "CountryOfOrigin";
+            column.Name = "Country";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "SensorType";
+            column.Name = "Sensor type";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "SensorResolution";
+            column.Name = "Sensor resolution";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "LensType";
+            column.Name = "Lens type";
+            column.Width = 180;
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "VideoFormat";
+            column.Name = "Video format";
+            gvCameras.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "WeatherSealing";
+            column.Name = "Weather sealing";
+            gvCameras.Columns.Add(column);
+
+            bindSrcCameras.Add(new Camera("Canon", "EOS R5", "Japan", "CMOS", 45, "Interchangeable lens", "8K DCI", true));
+            EventArgs args = new EventArgs(); OnResize(args);
+        }
+
+        private void fMain_Resize(object sender, EventArgs e)
+        {
+            int buttonsSize = 5 * btnAdd.Width + 2 * tsSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonsSize, 0, 0, 0);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Camera camera = new Camera();
+
+            fCamera ft = new fCamera(camera);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcCameras.Add(camera);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            Camera camera = (Camera)bindSrcCameras.List[bindSrcCameras.Position];
+
+            fCamera ft = new fCamera(camera);
+            if (ft.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcCameras.List[bindSrcCameras.Position] = camera;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Delete the current record?", "Delete record",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                bindSrcCameras.RemoveCurrent();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Clear the table?\n\nAll data will be lost", "Data cleaning",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                bindSrcCameras.Clear();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Close the application?", "Exit the program",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
